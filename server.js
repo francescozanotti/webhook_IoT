59246d20f95ed42589846416fdf02e4e08e5d3a0
francescozanotti <f.zanotti@adhox.it>
2025-02-12 15:19:43 +0100

    custom railay port

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
